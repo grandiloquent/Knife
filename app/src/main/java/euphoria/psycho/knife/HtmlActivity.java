22f27ef5efcabc3801af35a852505e8ee52cf4ad
grandiloquent <1785907865@qq.com>
2020-05-29 03:48:32 +0800

    changed

--- app/src/main/java/euphoria/psycho/knife/HtmlActivity.java
+++ app/src/main/java/euphoria/psycho/knife/HtmlActivity.java
@@ -1,11 +1,20 @@
 package euphoria.psycho.knife;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 
 import androidx.annotation.Nullable;
@@ -13,6 +22,29 @@ import androidx.annotation.Nullable;
 public class HtmlActivity extends Activity {
     private WebView mWebView;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWebView = new WebView(this);
+        setContentView(mWebView);
+        mWebView.loadUrl(getIntent().getData().toString());
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+            }
+        });
+
+        registerForContextMenu(mWebView);
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+    }
 
     @Override
     public void onBackPressed() {
@@ -24,27 +56,29 @@ public class HtmlActivity extends Activity {
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Context context;
-        mWebView = new WebView(this);
-        setContentView(mWebView);
-        mWebView.loadUrl(getIntent().getData().toString());
+    public void onCreateContextMenu(ContextMenu contextMenu, View v, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(contextMenu, v, menuInfo);
+        final WebView.HitTestResult webViewHitTestResult = mWebView.getHitTestResult();
 
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                view.scrollTo(0, PreferenceManager.getDefaultSharedPreferences(HtmlActivity.this)
-                        .getInt("scrollY", 0));
-            }
-        });
-    }
+        if (webViewHitTestResult.getType() == WebView.HitTestResult.IMAGE_TYPE ||
+                webViewHitTestResult.getType() == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
 
-    @Override
-    protected void onPause() {
-        PreferenceManager.getDefaultSharedPreferences(this).edit()
-                .putInt("scrollY", mWebView.getScrollY()).apply();
-        super.onPause();
+            contextMenu.setHeaderTitle("保存图片");
+
+            contextMenu.add(0, 1, 0, "Save - Download Image")
+                    .setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+
+                            String image = webViewHitTestResult.getExtra();
+
+
+                            Log.e("TAG/", "Debug: onMenuItemClick, \n" + image);
+
+
+                            return false;
+                        }
+                    });
+        }
     }
 }
